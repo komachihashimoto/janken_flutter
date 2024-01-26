f7b54388cf86f13a12a7f6ea6aeee375708c7cf7
komachihashimoto <joujinameko@gmail.com>
2024-01-26 15:03:35 +0900

    ベース

--- lib/main.dart
+++ lib/main.dart
@@ -45,18 +45,30 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('じゃんけんアプリ'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              '相手',
+              style: TextStyle(fontSize: 30),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '✌',
+              style: TextStyle(fontSize: 80),
+            ),
+            SizedBox (
+              height: 80,
+            ),
+            Text(
+              '自分',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '✊',
+              style: TextStyle(fontSize: 170),
             ),
           ],
         ),
